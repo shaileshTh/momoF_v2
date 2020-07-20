0fbd18430ea0ed0fa06c134461a395051008e0b3
Shailesh Thapa <shaileshthapa999@gmail.com>
2020-07-20 17:53:31 -0400

    modal

--- src/components/DishCard.js
+++ src/components/DishCard.js
@@ -5,13 +5,31 @@ import Button from 'react-bootstrap/Button'
 import {formatPrice} from '../utils/format'
 import {CartContext} from '../context/CartContext'
 import Badge from 'react-bootstrap/Badge'
+import Modal from 'react-bootstrap/Modal'
 
 const DishCard = ({dish}) => {
+  
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
     const [quantity, setQuantity] = useState(1);
     const {addToCart} = useContext(CartContext)
 
     return(
+        <>
+          <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+              <Modal.Title>Added to Cart!</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>Click on Cart Button on menu bar to checkout.</Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Okay
+            </Button>
+          </Modal.Footer>
+        </Modal>
         <ListGroup variant="flush" className = "text-center">
             <ListGroup.Item as = "h3" key = {dish.id}style = {{lineHeight:'1.5em', marginBottom:'15px', borderRadius:'10%'}}>
               {dish.title}{' '}<Badge variant="secondary">{formatPrice(dish.price_in_cents)}</Badge>{' '}
@@ -21,11 +39,15 @@ const DishCard = ({dish}) => {
                 onChange = {(event) => setQuantity(event.target.value)}
                 style = {{height: '28px', width:'40px',textAlign:'center'}}
             /></small>{' '}
-              <Button onClick = {() => addToCart(dish, quantity)} variant="dark">
+              <Button onClick = {() => {
+                  addToCart(dish, quantity)
+                  handleShow()
+                }} variant="dark">
               <Emoji symbol="🛒" label="cart"/>Add to Cart
               </Button>
             </ListGroup.Item>
           </ListGroup>
+        </>
     )
 }
 
