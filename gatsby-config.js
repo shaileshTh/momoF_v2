107323e77557db0e6ca7d97430e4c8d996dedab6
Shailesh Thapa <shaileshthapa999@gmail.com>
2020-07-20 01:49:55 -0400

    undo

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
     {
       resolve: `gatsby-source-strapi`,
       options: {
-        apiURL: process.env.API_URL,
+        apiURL: process.env.API_URL || `http://localhost:1337`,
         contentTypes: ['dish'],
       }
     },
